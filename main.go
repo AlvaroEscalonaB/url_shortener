6e1992b80dcb798f9874a2af94f0e0f32d8bd8b6
Alvaro Escalona <escalona.alvaro.13@gmail.com>
2025-02-15 19:49:39 -0300

    Init: Add basic routes

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+
+	"url_shortener/views"
+)
 
 func main() {
-	fmt.Println("Hi!")
+	component := views.Index()
+
+	//mux := http.NewServeMux()
+
+	// mux.HandleFunc("GET /static/", view.ServeStaticFiles)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
 }
