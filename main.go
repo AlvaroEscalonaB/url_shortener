9e10f599207c2ede138e7efffb01a552a3a83eb7
Alvaro Escalona <escalona.alvaro.13@gmail.com>
2025-02-15 23:43:52 -0300

    Replace normal handler for a multiplexer

--- main.go
+++ main.go
@@ -12,12 +12,13 @@ import (
 func main() {
 	component := views.Index()
 
-	//mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
 	// mux.HandleFunc("GET /static/", view.ServeStaticFiles)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	http.Handle("/", templ.Handler(component))
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	mux.Handle("/", templ.Handler(component))
+	// http.Handle("/", )
 
 	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", mux)
 }
